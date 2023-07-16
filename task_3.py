4cf4d4f02e39fc24e843e7fe1180b50ced1b0fdf
unknown <smtishkar@mail.ru>
2023-07-16 20:33:18 +0300

    отформатирован код

--- task_3.py
+++ task_3.py
@@ -3,29 +3,21 @@
 # Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
 
 num = int(input("Введите число для проверки: "))
-# num = 89
-
-
 MAX_VALE =100_000
-
 
 def num_check (num, MAX_VALUE):
     count = 0
     for i in range(1,MAX_VALE):
-        if num % i ==0:
-            count +=1 
-
+        if num % i == 0:
+            count += 1 
     return count
 
 
-while num <=0 or num > MAX_VALE:
+while num <= 0 or num > MAX_VALE:
     print("введено не корретное число")
     num = int(input("Введите число для проверки: "))
-if num >0 and num < MAX_VALE:
-    if num_check(num, MAX_VALE) >2:
+if num > 0 and num < MAX_VALE:
+    if num_check(num, MAX_VALE) > 2:
         print("число составное")
     else:
         print("число простое")
-# else:
-#     print("введено не корретное число")
-# print(num_check(num, MAX_VALE))
